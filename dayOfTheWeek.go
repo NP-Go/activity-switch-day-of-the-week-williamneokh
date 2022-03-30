451da3ff5cea9bd3188b17cad99fd24d9f8a7e80
williamneo <williamneokh@gmail.com>
2022-03-31 00:43:42 +0800

    Done

--- dayOfTheWeek.go
+++ dayOfTheWeek.go
@@ -1,5 +1,37 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 func main() {
 	//Insert code here
+	var dayNum int
+	today := time.Now().Weekday()
+	fmt.Println(today)
+
+	switch time.Now().Weekday() {
+	case time.Monday:
+		dayNum = 1
+	case time.Tuesday:
+		dayNum = 2
+	case time.Wednesday:
+		dayNum = 3
+	case time.Thursday:
+		dayNum = 4
+	case time.Friday:
+		dayNum = 5
+	case time.Saturday:
+		dayNum = 6
+	case time.Sunday:
+		dayNum = 7
+
+	}
+	if dayNum%2 == 0 {
+		fmt.Println("It's an even number")
+	} else {
+		fmt.Println("It's an odd number")
+	}
+
 }
